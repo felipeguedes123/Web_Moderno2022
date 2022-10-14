642bf3249cdb54d9e9bdc47a04a266c412f38838
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-14 13:12:46 -0300

    Comentario
    Prox. Aula 20- Bloco de Código

---
+++ FUNDAMENTOS/Inicio/comentario.js
@@ -0,0 +1,9 @@
+// Console.log é exibido para exibir algo na tela do computador
+console.log("Passo 01") 
+console.log("Passo 02")
+// console.log("Passo 03") linha comentada nao sera executada
+
+
+/*Comentario com mais 
+ * de uma linha 
+ */
---
+++ FUNDAMENTOS/Inicio/sentenca_2.js
@@ -0,0 +1,8 @@
+// console.log(1 + 3);
+// console.log("Aspas dupla");
+// console.log('Aspas simples');
+console.log("Passo 01") 
+console.log("Passo 02")
+console.log("Passo 03")
+
+// Meio de anotação escrevendo anotação
