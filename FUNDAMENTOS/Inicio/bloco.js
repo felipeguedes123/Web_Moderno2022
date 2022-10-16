56707e96f10996d8a1ff5b5c7d6357081c41c515
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-16 15:50:05 -0300

    Desafio Sentença e Blooo
    Prox aula 23

---
+++ FUNDAMENTOS/Inicio/bloco.js
@@ -0,0 +1,35 @@
+
+{
+
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+    
+}
+
+
+
+{
+
+    console.log("Passo #04");
+    console.log("Passo #05");
+
+    {
+        {
+            {
+                {
+                    {
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+console.log("FIM!!")
+
+// Iremos organizar os códigos em pastas, arquivos, e dentro desse arquivo teremos sentença de códigos juntamente com os blocos
