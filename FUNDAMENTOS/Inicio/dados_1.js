136808458c15df925745ec4608b16b2267996969
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-16 20:55:19 -0300

    Trabalhando com dados 2
    Aula 23 trabalhando com dados 03

---
+++ FUNDAMENTOS/Inicio/dados_1.js
@@ -0,0 +1,24 @@
+var nome = "Caneta"  // forma mais moderna vc substiui (var) por (let) , existem algumas diferenças entre elas mas ainda veremos mais a frente.
+let quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(10); //valor literal
+console.log(preco);
+console.log(6.4);
+console.log(imposto)
+console.log(1.5);
+
+console.log(preco + imposto);
+console.log(7.9);
+
+nome = "Caneta BIC";
+
+console.log(precoFinal)
+console.log(nome)
+
+
+
---
+++ FUNDAMENTOS/Inicio/dados_2.js
@@ -0,0 +1,25 @@
+console.log(19.90 * 0.60)
+
+
+let preco = 19.90;
+let desconto = 0.4;
+
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto)
+
+
+//Anatomia de como uma variável é declarada
+
+let nome = "Caderno"; // é uma variável string -> sequência de símbolos
+let categoria = "Palelaria";
+
+console.log("Produto: " + nome
+ + ", Categoria: "+ categoria 
+ + ", Preço: " + preco 
+ + ", Desconto: " + desconto);
+
+ console.log("1"+1); 
+
+ //escolhas no nome da variavel tem que ser bom para trazer clareza  no código desenvolvido
+
+ 
