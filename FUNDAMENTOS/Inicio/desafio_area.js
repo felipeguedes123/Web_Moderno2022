be846398717fa63acf19d304ad3bdb1df488c4e2
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-17 18:11:35 -0300

    Aula 34
    Próxima aula 35 organização básica de um código JS

---
+++ FUNDAMENTOS/Inicio/desafio_area.js
@@ -0,0 +1,6 @@
+// PI * raio * raio
+
+let raio = 10;
+let areaCircuferencia = Math.PI * raio * raio;
+
+console.log(areaCircuferencia + "m2.")
---
+++ FUNDAMENTOS/Inicio/desafio_troca.js
@@ -0,0 +1,14 @@
+let a = 7;
+let b = 94;
+
+// depois da troca .....a = 94 e b = 7
+
+// let temp = a;
+// a = b;
+// b = temp;
+[a, b] = [b, a] // outra forma possível
+
+
+
+console.log(a)
+console.log(b)
