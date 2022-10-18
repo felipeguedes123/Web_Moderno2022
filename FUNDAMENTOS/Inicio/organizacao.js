74ac6a7e1ffec4497e92ac7c9e5f1a208cd64e90
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-18 11:35:42 -0300

    Usando Temp String
    Proxima aula 45

---
+++ FUNDAMENTOS/Inicio/organizacao.js
@@ -0,0 +1,12 @@
+console.log("Sentença de código")
+
+{
+    {
+        {
+            console.log("Ola")
+            console.log("Mundo!")
+        }
+    }
+}
+
+//JavaScrip é organizado por bloco e sequencia de código 
