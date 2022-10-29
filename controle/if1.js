390cefdfe640ceff2644d6ff8feee2fd5f62b69c
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-10-29 09:50:06 -0300

    Aula 78

---
+++ controle/if1.js
@@ -0,0 +1,33 @@
+function soBoaNoticia (nota) {
+    if(nota >= 7) {
+        console.log("Aprovado com " + nota)
+
+    }if(nota < 7) {
+        console.log("Reprovado com " + nota)
+    }
+}
+
+soBoaNoticia(8.1)
+soBoaNoticia(7)
+soBoaNoticia(6.9)
+
+
+
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log("É verdade... " + valor)
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo("")
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(" ")
+seForVerdadeEuFalo("?")
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1, 2])
+seForVerdadeEuFalo({})
