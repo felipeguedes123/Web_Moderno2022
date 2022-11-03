4c53fdf436f58e89177705986710e09d409697ce
Felipe Guedes <85907359+felipeguedes123@users.noreply.github.com>
2022-11-03 17:41:39 -0300

    Aula 95
    Prox - 96 arrow #02

---
+++ funcao/arrowFunction1.js
@@ -0,0 +1,21 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+dobro = (a) => {
+    return 2 * a
+}
+
+dobro = a => 2 * a // retur esta implícito
+
+
+console.log(dobro(Math.PI))
+
+
+let ola = function () {
+    return "Olá"
+}
+ola = () => "Olá"
+
+
+console.log(ola())
